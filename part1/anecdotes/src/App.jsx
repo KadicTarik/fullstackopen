c6a4645e0d35e650af7f4afe84e4c49d410710f7
KadicTarik <tarik.kadic1349@gmail.com>
2024-01-09 01:45:47 +0100

    part1b - anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+const Anecdote = ({anecdotes, points}) => {
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes}
+      <p>has {points} votes</p>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const MostVotes = (props) => {
+  const noVotes = props.points.every(item => item === 0)
+  if (noVotes === true) {
+    return (
+      <div>
+        <h1>Anecdote with the most votes</h1>
+        vote to display the most voted anecdote
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>Anecdote with the most votes</h1>
+      {props.anecdotes}
+      <p>has {props.max} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  const maxPoints = Math.max(...points)
+  const maxIndex = points.indexOf(maxPoints)
+
+  const handleNext = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  return (
+    <div>
+      <Anecdote anecdotes={anecdotes[selected]} points={points[selected]}/>
+      <Button onClick={handleVote} text='vote'/>
+      <Button onClick={handleNext} text='next anecdote'/>
+      <MostVotes points={points} anecdotes={anecdotes[maxIndex]} max={maxPoints}/>
+    </div>
+  )
+}
+
+export default App
